4e36b0458e93d13893da37b1896aafeb26cd14a2
anil91608 <anil91608@gmail.com>
2025-10-24 06:08:16 +0000

    Initial commit - Node.js app with Dockerfile

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello from Jenkins CI/CD with JDK 21!'));
+
+app.listen(3000, () => console.log('App running on port 3000'));
+
